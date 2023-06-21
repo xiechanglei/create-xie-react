f0a9b159b4a0c512841cec9cadabe95e6b0cb6e7
xiechanglei <709955748@qq.com>
2023-06-21 16:41:35 +0800

    build

--- template/src/ui/index.ts
+++ template/src/ui/index.ts
@@ -7,6 +7,8 @@ import "./font/font.css"
  * 通过initStyledConfig去动态设置 styled-px2vw-plugin的配置
  * 一般情况下，我们都是按照设计图的尺寸去实现代码
  */
+
+//http://view.jqueryfuns.com/2023/3/6/499645bc04e396f0b7bf573bdbe46f0d/ui-buttons.html
 export const initStyledComponent = () => {
     const windowWidth = document.body.clientWidth
     // initStyledConfig({viewportWidth: windowWidth, ignoreAttrs: []})
